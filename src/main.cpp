26e7f549284dc9d1a20097a4d6d8b09db46f07d6
dafners <a25110283@ceti.mx>
2026-04-24 01:39:18 +0000

    Práctica 4

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <Foco.hpp>
+#include <SerieFocos.hpp>
 
 using namespace std;
 
 int main(int argc, char const *argv[]) 
 {
-    Foco foquito;
-    foquito.Encender();
-    foquito.Apagar();
+    SerieFocos serie;
+    serie.EncenderFocos();
+    serie.MostrarFocos();
+    serie.ApagarFocos();
+    serie.MostrarFocos();
 
-    cout << "Estado Foco:  " << foquito.LeerEstado() << endl;
     return 0;
 }
 
